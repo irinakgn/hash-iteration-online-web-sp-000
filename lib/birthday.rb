84fab9012a8a57f058bd5f33ebd504e9c2303e1e
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 23:08:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,18 +9,3 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{key}! You are now #{value} years old!"
   end
 end
-
-
-# def select_winner(passengers)
-#   passengers[0]
-# end
-
-def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |key, value|
-      if value < 12
-        puts "Happy Birthday #{key}! You are now #{value} years old!"
-      else
-        puts "You are too old for this."
-      end
-  end
-end
