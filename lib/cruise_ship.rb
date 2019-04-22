b2436c41000662002b3eff4495ed79c493a2da15
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 23:11:20 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,21 @@
 # }
 
 def select_winner(passengers)
-  passengers
+  passengers.find  { |i| i % 5 == 0 and i % 7 == 0 }
+end
+
+
+
+# def select_winner(passengers)
+#   passengers[0]
+# end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |key, value|
+      if value < 12
+        puts "Happy Birthday #{key}! You are now #{value} years old!"
+      else
+        puts "You are too old for this."
+      end
+  end
 end
